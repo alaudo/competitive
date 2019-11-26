e162fe330bd35313efd3401dfc90b0cf74d38d65
Alexander Galkin (BING USA) <algalkin@microsoft.com>
2019-11-26 20:11:34 +0100

    Added configs

--- Python/ACMSGURU/102.py
+++ Python/ACMSGURU/102.py
@@ -1,30 +1,26 @@
-a = int(input())
-dividers = []
-divisor = []
-coprimes = []
-#for i in range(a):
-    #while True:
-        #if (a % i) == 0 and a >= i:
-            #dividers.append(a/i)
-        #else:
-            #break
-for i in range(a-2):
-    i = i+2
-    dividers.append(i)
+# a = int(input())
 from math import sqrt
+
+# a = int(input())
+a = 10000
+b = a
+dividers = []
+    
+
 while True:
-    for i in range(2,int(sqrt(a)+1)):
-        if (a % i) == 0:
-            a = a//i
-            if not(i in divisor):
-                divisor.append(i)
+    for i in range(2,b+1):
+        if (b % i) == 0:
+            if not(i in dividers):
+                dividers.append(int(i))
+            b = b // i
             break
-    if a == 1:
+    if  b == 1:
         break
-for d in range(len(divisor)):
-    if dividers[i] % divisor[d] == 0:
-        break
-    else:
-        coprimes.append(dividers[i])
-print(len(coprimes))
+
+allnums = list(range(a))
+for d in dividers:
+    allnums = list(filter(lambda x: x % d != 0, allnums))
+
+print(len(allnums))
+
         
